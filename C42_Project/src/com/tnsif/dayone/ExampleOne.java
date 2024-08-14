d9b8f76572f71f5e40446d5beac8d92442ff7620
Jaisurya <Jaisurya@DESKTOP-2U2K1TG>
2024-08-14 10:13:05 +0530

    commit 1

---
+++ C42_Project/src/com/tnsif/dayone/ExampleOne.java
@@ -0,0 +1,11 @@
+package com.tnsif.dayone;
+
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
